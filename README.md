5d7e94437f0962e6040eca81a8d21091cbaa980d
Vincent Jauneau <vincent.jauneau@platypus-creation.com>
2013-03-28 19:13:11 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+http://box.com python API
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='box',
+      version='0.1',
+      description='box.com python librairy',
+      author='Vincent Jauneau',
+      author_email='vincent.jauneau@platypus-creation.com',
+      url='https://github.com/platypus-creation/box.com-python',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['git+git://github.com/requests/requests-oauthlib.git']
+)
